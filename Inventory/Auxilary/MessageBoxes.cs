66de57d7d0516b39daf2f277b8cfe377c46a36ff
engy-elbialy <eng.engy_elbialy@yahoo.com>
2018-10-05 11:17:51 +0200

    Add MessageBoxes Class

---
+++ Inventory/Auxilary/MessageBoxes.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Inventory.Auxilary
+{
+    class MessageBoxes
+    {
+        public void SuccessMsgBox(string Text)
+        {
+            MessageBox.Show(Text, "Inventory", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
