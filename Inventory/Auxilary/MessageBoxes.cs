db3ca352363ccefbc37cdcd927f53bf9c920dbe9
AhmedNagyHassaan <ahmed.nagy.hassaan@live.com>
2018-10-05 11:41:13 +0200

    Add SuccessMsgBox Func

--- Inventory/Auxilary/MessageBoxes.cs
+++ Inventory/Auxilary/MessageBoxes.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Windows.Forms;
 
 namespace Inventory.Auxilary
 {
     class MessageBoxes
     {
+        public void SuccessMsgBox(string text)
+        {
+            MessageBox.Show(text, "Inventory", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
 }
