8e8b0c11d99ed182668c34ab17658cf080116836
AhmedNagyHassaan <ahmed.nagy.hassaan@live.com>
2018-10-07 17:05:43 +0200

    BaseForm for all App Classes

---
+++ Inventory/Auxilary/BaseForm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Inventory
+{
+    internal class BaseForm : System.Windows.Forms.Form
+    {
+        public override string Text
+        {
+            get { return base.Text.Remove(0, base.Text.IndexOf('-')+1); }
+            set { base.Text = $"{Properties.Settings.Default.ApplicationName} - {value}"; }
+        }
+    }
+}
+
